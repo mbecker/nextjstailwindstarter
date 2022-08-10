2f03561546bc4f573bb6dd6e178ca76b4ea53c11
Mats Becker <mats.becker@gmail.com>
2022-08-10 09:12:49 +0200

    Add svg wave

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,12 +13,37 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </main>
       </div>
-      <div className="bg-sky-500 h-12 relative bottom-0 -mb-4 shadow-sky-200 shadow origin-center rotate-1 scale-105 skew-x-0 skew-y-0 w-full min-w-min"></div>
-      <footer className="z-10 footer py-4 px-4">
+      {/* <div className="bg-sky-500 h-12 relative bottom-0 -mb-4 shadow-sky-200 shadow origin-center rotate-1 scale-105 skew-x-0 skew-y-0 w-full min-w-min"></div> */}
+
+
+      
+      <footer className="z-10 footer">
+      <div
+  className="relative"
+>
+<svg
+  className="w-full h-[full] -mb-2"
+  viewBox="0 0 1441 100"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlnsXlink="http://www.w3.org/1999/xlink"
+>
+  <path
+    className="fill-sky-500"
+    stroke="none"
+    d="M -0 44.555557 L 1441 1 L 1441 99 L -0 99 Z"
+  />
+</svg>
+
+
+
+
+
+</div>
+        <div className="py-4 px-4 bg-sky-500">
         <div className="container sm:flex justify-between">
           <div className="mb-6 sm:mb-0 sm:flex">
             <p>Copyright © 2022 Tailwind Labs Inc.</p>
-            <p className="sm:ml-4 sm:pl-4 sm:border-l sm:border-slate-200 dark:sm:border-slate-200/5">
+            <p className="sm:ml-4 sm:pl-4">
               <a className="link-footer" href="/brand">
                 Trademark Policy
               </a>
@@ -30,6 +55,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           >
             Edit this page on GitHub
           </a>
+        </div>
         </div>
       </footer>
 
